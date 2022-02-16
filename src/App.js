c87e6b67889abf8371d5a1b83b681166f162d7a5
Piotr Stefanski <xmestre@wp.pl>
2022-02-16 22:10:06 +0000

    working with API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import { useState, useEffect } from 'react';
 import './App.css';
 
+// https://api.github.com/users/PiotrStef84
 
-function App() {
-  const [emotion, setEmotion] = useState("happy");
-  const [secondary, setSecondary] = useState("tired");
-  
-  useEffect(() => {console.log(`It's ${emotion} around here!`)},[emotion])
 
-  useEffect(() => {console.log(`It's also ${secondary} around here!`)})
+function App({login}) {
+const [data, setData] = useState(null)
+const [loading, setLoading] = useState(false)
+const [error, setError] = useState(null)
   
-  return(
-  <>
-  <h1>Current emotion is {emotion} and {secondary}</h1>
-  <button onClick={() => setEmotion("happy")}>Happy</button>
-  <button onClick={() => setEmotion("frustrated")}>Frustrate</button>
-  <button onClick={() => setEmotion("relaxed")}>Relax</button>
-  <hr />
-  <button onClick={() => setSecondary("groovy")}>Groovy</button>
-  </>
-  )}
+useEffect(() => {
+  if(!login) return;
+  setLoading(true);
+  fetch(`https://api.github.com/users/${login}`)
+  .then((response) => response.json())
+  .then(setData)
+  .then(() => setLoading(false))
+  .catch(setError);
+}, [login]);
+
+if(loading) return <h1>Loading...</h1>;
+if(error) return <pre>{JSON.stringify(error, null, 2)}</pre>
+if(!data) return null;
+
+
+  return <div>
+    <h1>{data.login}</h1>
+    <p>{data.location}</p>
+    <img alt={data.login} src={data.avatar_url}/>
+    </div>;
+}
+
 
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App'
 
 
 ReactDOM.render(
-  <App/>
+  <App login="PiotrStef84"/>
   , document.getElementById('root')
 );
 
